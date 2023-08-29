4fcad4375e3ed397828c059d7f514be769de1dfa
PhungDucManhPhungDucManh <phungducmanhph@gmail.com>
2023-08-29 14:57:46 +0700

    init project

---
+++ src/main/java/com/httm/WebBanThuocTay/WebBanThuocTayApplication.java
@@ -0,0 +1,13 @@
+package com.httm.WebBanThuocTay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBanThuocTayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBanThuocTayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/httm/WebBanThuocTay/WebBanThuocTayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.httm.WebBanThuocTay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBanThuocTayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
